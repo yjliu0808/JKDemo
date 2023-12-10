74bdcc9167f35058991fbc0d5f87a88b5d214a89
yjliu0808 <yjliu0808@163.com>
2023-12-10 21:07:50 +0800

    更新到最新

--- run_test.py
+++ run_test.py
@@ -13,13 +13,7 @@ from BeautifulReport import BeautifulReport
 if __name__ == '__main__':
     # 1、收集用例并返回 测试套件，testcases用例文件夹，返回能够找到的所有用例，封装成一个测试套件
     ts = unittest.TestLoader().discover('testcases')
-    # 运行器运行并生成报告
-    # with open('reports\mall项目接口测试报告.html','wb') as f:
-    #     runner = HTMLTestRunner(stream=f)
-    #     runner.run(ts)
+
     br = BeautifulReport(ts)
-    # br.report(description='mall项目',filename = 'reports/mall接口测试报告.html')
-    # ts = unittest.TestLoader().discover('testcases')
-    # br = BeautifulReport(ts)
-    # br.report(**config['report'])
+
     br.report(**settings.REPORT_CONFIG)
